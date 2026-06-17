05aedbbb55845b0a0c863f5c12a58d822c1f5f18
marwoj17 <max.wojcikiewicz666@gmail.com>
2026-06-17 12:11:34 +0200

    Update Collection.cs

--- Models/Collection.cs
+++ Models/Collection.cs
@@ -4,10 +4,7 @@ namespace CollectionSystem.Models
 {
     public class Collection
     {
-        // Klasa reprezentująca pojedynczą kolekcję
-        // Zawiera nazwę kolekcji oraz listę elementów tej kolekcji
         public string Name { get; set; }
-        // Lista elementów w kolekcji. Domyślnie inicjalizowana jako pusta lista.
         public List<CollectionItem> Items { get; set; } = new List<CollectionItem>();
     }
 }
