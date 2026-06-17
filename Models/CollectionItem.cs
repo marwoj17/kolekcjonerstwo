44e3d89e4a199b06623352f530221b2f3deee81e
marwoj17 <max.wojcikiewicz666@gmail.com>
2026-06-17 12:11:56 +0200

    Remove comments from CollectionItem class

--- Models/CollectionItem.cs
+++ Models/CollectionItem.cs
@@ -2,10 +2,7 @@
 { 
     public class CollectionItem
     {
-        // Pojedynczy element w kolekcji
-        // Title: tytuł / nazwa elementu
         public string Title { get; set; }
-        // Description: dodatkowe informacje o elemencie
         public string Description { get; set; }
     }
 }
